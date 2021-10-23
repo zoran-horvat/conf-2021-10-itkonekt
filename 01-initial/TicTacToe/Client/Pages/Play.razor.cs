9aed3c30ff8bd05337ae269c1d513fd9e93202cf
Zoran Horvat <zh@codinghelmet.com>
2021-10-24 01:35:29 +0200

    Added initial and final code

---
+++ 01-initial/TicTacToe/Client/Pages/Play.razor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using TicTacToe.Shared;
+
+namespace TicTacToe.Client.Pages
+{
+    public partial class Play
+    {                                                                                                                                                                                                   // snp02 Instantiate a board in the UI
+        private void Restart()                                                                                                                                                                          // snp15.3 Next command is to restart the game
+        {                                                                                                                                                                                               // snp20 Restart the game
+        }
+
+        private void MakeMove(int row, int col)                                                                                                                                                         // snp15.4 And the last command is to make a move
+        {                                                                                                                                                                                               // snp21 Make the move on the board
+        }
+    }
+}
---
+++ 01-initial/TicTacToe/Shared/Board.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace TicTacToe.Shared
+{
+    public class Board                                                                                                                                                                                  // snp22 Define a record type for cells // snp01.1 We are expected to develop this model class
+    {                                                                                                                                                                                                   // snp03 Use tuples for quick prototyping // snp02.1 How do we represent move played?
+    }
+}
